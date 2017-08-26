a4b3d57d923533a19f8753a41a1cbd541fdd8958
scolia <scolia@qq.com>
2017-08-26 12:05:02 +0800

    fix, 添加GetHashCode的实现

--- Core/ABData.cs
+++ Core/ABData.cs
@@ -25,7 +25,13 @@
 
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+            // 重写Equals时一般也要重写GetHashCode, 且要保证Equals成立时, HashCode也相等, 反之不然.
+            // 这里是模仿jdk5.0中, String类的相应实现.
+            int prime = 31; // 一般取一个质数
+            int result = 1;
+            result = prime * result + (Name == null ? 0 : Name.GetHashCode());
+            result = prime * result + (Hash == null ? 0 : Hash.GetHashCode());
+            return result;
         }
 
         public override string ToString()
